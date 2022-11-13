99f211e017213387dbad31a57b0261934df07bf6
damanM <daman.m@outlook.com>
2022-11-13 12:45:30 -0500

    added event listener on dropdown

--- appApi.js
+++ appApi.js
@@ -1,4 +1,3 @@
-
 // on page load  -  have static img and placeholder text with dropodwn menu above it
 // addeventlistener to dropdown and update page according to user choice by replacing the static img and placeholder text with api data
 // the fetch call will be exceuted within the addeventlistener 
@@ -8,53 +7,49 @@
 const ramApp = {};
 
 
-ramApp.getCharacter = (userSelection) => {
+ramApp.getCharacter = (selection) => {
 
     const url = new URL('https://rickandmortyapi.com/api/character');
 
     // Search Params
     url.search = new URLSearchParams ({
         id: 1
-        
     })
 
-    fetch(url)
-    .then( (res) => {
-        return res.json();
-    })
-    .then ((data) =>{
-        // console.log(data.results)
-        document.querySelector('#characterCard')
-        // .innerHTML = "";
-        console.log(data.results[0].name)
-        // const characterOptions = data.results[userSelection].name
-        
+    const character = document.querySelector('#character');
+    
+    // event listener on dropdown menu
+    character.addEventListener('change', function() {
+
+        fetch(url)
+        .then( (res) => {
+            return res.json();
+        })
+        .then ((data) =>{
+            console.log(data.results);
+             selection = data.results[this.value].name;
+            console.log(selection);
+            const characterImg = data.results[this.value].image;
+            const characterSpeices = data.results[this.value].species;
+            const characterCard = document.querySelector('#characterCard');
+
+            characterCard.innerHTML = `<div class="imgContainer"> 
+            <img src="${characterImg}" alt="Rick">
+        </div>`;
 
 
-        
+        })
+
+        return selection
     })
 }
 
-// created var for dropdown menu
-const character = document.querySelector('#character');
-
-//addevent listener on the dropdown
-character.addEventListener('change', function() {
-    
-    // function to display user selection
-    const userSelection = this.value
-    console.log(userSelection)
-    ramApp.getCharacter(userSelection)
-
-    
-    })
+ramApp.getCharacter();
 
 
+console.log('featuretest')
 
 ramApp.init = () => {
-    // console.log('yea');
-    ramApp.getCharacter()
+
+
 }
-
-
-ramApp.init()
