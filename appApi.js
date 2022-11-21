cca178fa365a5a17a783a795ed77ec85eced7f89
damanM <daman.m@outlook.com>
2022-11-20 21:53:42 -0500

    made app responsive, took out stretch goals, added finishing css touches to character card and got rid of console logs :sunglasses:

--- appApi.js
+++ appApi.js
@@ -7,85 +7,61 @@
 const ramApp = {};
 
 
+
+
+const dropdown = document.querySelector('#character');
+
 ramApp.getCharacter = (selection) => {
 
-    const url = new URL('https://rickandmortyapi.com/api/character/1,2,3,4,5,242,89');
-    const dropdown = document.querySelector('#character');
-    // const urlEp = new URL('https://rickandmortyapi.com/api/episode');
+    const url = new URL('https://rickandmortyapi.com/api/character/1,2,3,4,5');
+    
+    
 
-    // Search Params
-    // url.search = new URLSearchParams ({
-       
-    // })
-
-    // console.log(url.search)
-    // event listener on dropdown menu
-   const fetchCall = dropdown.addEventListener('change', function() {
+ dropdown.addEventListener('change', function() {
 
         fetch(url)
         .then( (res) => {
             return res.json();
         })
         .then ((data) =>{
-            console.log(data);
 
 
             // save user selection to parameter 
             selection = data[this.value].name;
-            console.log(selection);
 
-            //display selected img
+            //make vars for character data
             const characterImg = data[this.value].image;
-            // Display selected name
             const characterName = data[this.value].name;
             const characterSpecies = data[this.value].species;
             const characterGender = data[this.value].gender;
-            const firstEp = data[this.value].episode[0];
             const characterCardEl = document.querySelector('#characterCard');
             const characterStatus = data[this.value].status
             const characterLocation = data[this.value].location.name
 
-            
+            //display character data
             characterCardEl.innerHTML = `<div class="imgContainer"> <!-- imgContainer -->
-            <img src="${characterImg}" alt="Rick">
+            <img src="${characterImg}" alt="${characterName}">
             </div> 
             <div class="characterInfo">
             <p class="name">${characterName}</p>
-            <p class="status">${characterStatus}</p>
+            <span id="icon" class="icon"></span><p class="status" id="status">${characterStatus}</p>
             <div class="inline">
             <p>${characterSpecies}</p>
             <p>${characterGender}</p>
             </div>
-            <p>Last Seen: ${characterLocation}</p>
+            <p>Last Known Location: ${characterLocation}</p>
             </div>`;
-        
-            
-            return data
-            // const characterSpeices = data.results[this.value].species;
-            
-            // statusToggle = () => {
-            //     const  statusIcon = document.querySelector('.status::before');
-            //     if (characterStatus === 'Alive') {
-            //         statusIcon.style.color = 'red'
-            //     }
-            // }
-            // statusToggle()
-        })
 
-        
-        console.log(fetchCall)
+            
+        })
     })
 }
 
 
- 
-
 ramApp.getCharacter();
 
 
+   
 
-ramApp.init = () => {
-
-}
 
 
